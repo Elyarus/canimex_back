14148e1d6d2d6d9205bab0d88f2e530ec3295f7c
Elyarus <axeldelanda@gmail.com>
2024-07-10 00:42:20 -0300

    agregado metodo put y delete

--- src/main/java/com/canimex/Controlador.java
+++ src/main/java/com/canimex/Controlador.java
@@ -15,10 +15,10 @@ Servlets son clases Java que se ejecutan en un servidor de aplicaciones o un con
 
 Tomcat es un contenedor de servlets y servidor de aplicaciones web que ejecuta aplicaciones web basadas en Java.
  */
-// Clase Controlador: Maneja las peticiones HTTP para insertar y recuperar películas.
+// Clase Controlador: Maneja las peticiones HTTP para insertar y recuperar animes.
 @WebServlet("/animes") // Anotación que mapea este servlet a la URL "/animes"
 public class Controlador extends HttpServlet { // Declaración de la clase Controlador que extiende HttpServlet
-    // Método POST para insertar una nueva película desde una solicitud JSON
+    // Método POST para insertar una nueva anime desde una solicitud JSON
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Configurar cabeceras CORS
         response.setHeader("Access-Control-Allow-Origin", "*"); // Permitir acceso desde cualquier origen
@@ -31,7 +31,7 @@ public class Controlador extends HttpServlet { // Declaración de la clase Contr
             ObjectMapper mapper = new ObjectMapper();  // Crear un objeto ObjectMapper para convertir JSON a objetos Java
             Anime anime = mapper.readValue(request.getInputStream(), Anime.class);  // Convertir el JSON de la solicitud a un objeto Anime
         
-            // Consulta SQL para insertar una nueva película en la tabla 'peliculas'
+            // Consulta SQL para insertar un nuevo anim en la tabla 'anims'
             String query = "INSERT INTO animes (titulo, genero, duracion, imagen) VALUES (?, ?, ?, ?)";
             PreparedStatement statement = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);  // Indicar que queremos obtener las claves generadas automáticamente
         
@@ -43,14 +43,14 @@ public class Controlador extends HttpServlet { // Declaración de la clase Contr
         
             statement.executeUpdate();  // Ejecutar la consulta de inserción en la base de datos
         
-            // Obtener las claves generadas automáticamente (en este caso, el ID de la película)
+            // Obtener las claves generadas automáticamente (en este caso, el ID de la anime)
             ResultSet rs = statement.getGeneratedKeys();
             if (rs.next()) {
-                Long idPeli = rs.getLong(1);  // Obtener el valor del primer campo generado automáticamente (en este caso, el ID)
+                Long idAnime = rs.getLong(1);  // Obtener el valor del primer campo generado automáticamente (en este caso, el ID)
                 
-                // Devolver el ID de la película insertada como JSON en la respuesta
+                // Devolver el ID de la anime insertada como JSON en la respuesta
                 response.setContentType("application/json");  // Establecer el tipo de contenido de la respuesta como JSON
-                String json = mapper.writeValueAsString(idPeli);  // Convertir el ID a formato JSON
+                String json = mapper.writeValueAsString(idAnime);  // Convertir el ID a formato JSON
                 response.getWriter().write(json);  // Escribir el JSON en el cuerpo de la respuesta HTTP
             }
             
@@ -66,7 +66,7 @@ public class Controlador extends HttpServlet { // Declaración de la clase Contr
         }
     }
 
-    // Método GET para obtener todas las películas almacenadas en la base de datos y devolverlas como JSON
+    // Método GET para obtener todas los animes almacenadas en la base de datos y devolverlas como JSON
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Configurar cabeceras CORS
         response.setHeader("Access-Control-Allow-Origin", "*"); // Permitir acceso desde cualquier origen
@@ -76,16 +76,34 @@ public class Controlador extends HttpServlet { // Declaración de la clase Contr
         Connection conn = conexion.getConnection();  // Obtener la conexión establecida
 
         try {
-            // Consulta SQL para seleccionar todas las películas de la tabla 'animes'
-            String query = "SELECT * FROM animes";
-            Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);  // Ejecutar la consulta y obtener los resultados
+            // Consulta SQL para seleccionar todas las animes de la tabla 'animes'
+            // String query = "SELECT * FROM animes";
+            // Statement statement = conn.createStatement();
+            // ResultSet resultSet = statement.executeQuery(query);  // Ejecutar la consulta y obtener los resultados
+            String idParam = request.getParameter("id");
+            // String tituloParam = request.getParameter("titulo"); // titulo like %tituloParam%;
+            String query;
+            if (idParam != null) {
+                // Si se recibe un id, ajustar la consulta para seleccionar solo la anime con el id recibido
+                query = "SELECT * FROM animes WHERE id_anime = ?";
+            } else {
+                // Si no se recibe un id, seleccionar todas las animes
+                query = "SELECT * FROM animes";
+            }
+
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            if (idParam != null) {
+                statement.setInt(1, Integer.parseInt(idParam)); // Establecer el parámetro en la consulta preparada
+            }
+
+            ResultSet resultSet = statement.executeQuery();  // Ejecutar la consulta y obtener los resultados
 
             List<Anime> animes = new ArrayList<>();  // Crear una lista para almacenar objetos anime
 
             // Iterar sobre cada fila de resultados en el ResultSet
             while (resultSet.next()) {
-                // Crear un objeto Pelicula con los datos de cada fila
+                // Crear un objeto Anime con los datos de cada fila
                 Anime anime = new Anime(
                     resultSet.getInt("id_anime"),
                     resultSet.getString("titulo"),  
@@ -93,11 +111,11 @@ public class Controlador extends HttpServlet { // Declaración de la clase Contr
                     resultSet.getString("duracion"),
                     resultSet.getString("imagen")
                 );
-                animes.add(anime);  // Agregar el objeto Pelicula a la lista
+                animes.add(anime);  // Agregar el objeto Anime a la lista
             }
 
             ObjectMapper mapper = new ObjectMapper();  // Crear un objeto ObjectMapper para convertir objetos Java a JSON
-            String json = mapper.writeValueAsString(animes);  // Convertir la lista de películas a formato JSON
+            String json = mapper.writeValueAsString(animes);  // Convertir la lista de anime a formato JSON
 
             response.setContentType("application/json");  // Establecer el tipo de contenido de la respuesta como JSON
             response.getWriter().write(json);  // Escribir el JSON en el cuerpo de la respuesta HTTP
@@ -106,6 +124,95 @@ public class Controlador extends HttpServlet { // Declaración de la clase Contr
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);  // Configurar el código de estado de la respuesta HTTP como 500 (INTERNAL_SERVER_ERROR)
         } finally {
             conexion.close();  // Cerrar la conexión a la base de datos al finalizar la operación
+        }
+    }
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Configurar cabeceras CORS
+        response.setHeader("Access-Control-Allow-Origin", "*"); // Permitir acceso desde cualquier origen
+        response.setHeader("Access-Control-Allow-Methods", "*"); // Métodos permitidos get post put delete
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Cabeceras permitidas
+        Conexion conexion = new Conexion();  // Crear una nueva conexión a la base de datos
+        Connection conn = conexion.getConnection();  // Obtener la conexión establecida
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();  // Crear un objeto ObjectMapper para convertir JSON a objetos Java
+            Anime anime = mapper.readValue(request.getInputStream(), Anime.class);  // Convertir el JSON de la solicitud a un objeto Anime
+
+            // Consulta SQL para actualizar un anime en la base de datos
+            String query = "UPDATE animes SET titulo = ?, genero = ?, duracion = ?, imagen = ? WHERE id_anime = ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            // Establecer los parámetros de la consulta de actualización
+            statement.setString(1, anime.getTitulo());
+            statement.setString(2, anime.getGenero());
+            statement.setString(3, anime.getDuracion());
+            statement.setString(4, anime.getImagen());
+            statement.setInt(5, anime.getIdAnime());
+
+            // Ejecutar la consulta de actualización
+            int rowsUpdated = statement.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                response.setStatus(HttpServletResponse.SC_OK); // Configurar el código de estado de la respuesta HTTP como 200 (OK)
+                response.getWriter().write("{\"message\": \"Anime actualizado exitosamente.\"}");
+            } else {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND); // Configurar el código de estado de la respuesta HTTP como 404 (NOT FOUND)
+                response.getWriter().write("{\"message\": \"Anime no encontrado.\"}");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Imprimir el error en caso de problemas con la base de datos
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // Configurar el código de estado de la respuesta HTTP como 500 (INTERNAL SERVER ERROR)
+        } catch (IOException e) {
+            e.printStackTrace(); // Imprimir el error en caso de problemas de entrada/salida
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // Configurar el código de estado de la respuesta HTTP como 500 (INTERNAL SERVER ERROR)
+        } finally {
+            conexion.close(); // Cerrar la conexión a la base de datos al finalizar la operación
+        }
+    }
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Configurar cabeceras CORS
+        response.setHeader("Access-Control-Allow-Origin", "*"); // Permitir acceso desde cualquier origen
+        response.setHeader("Access-Control-Allow-Methods", "*"); // Métodos permitidos get post put delete
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Cabeceras permitidas
+        Conexion conexion = new Conexion();  // Crear una nueva conexión a la base de datos
+        Connection conn = conexion.getConnection();  // Obtener la conexión establecida
+
+        try {
+            String idParam = request.getParameter("id");  // Obtener el parámetro de consulta 'id'
+            if (idParam == null) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // Configurar el código de estado de la respuesta HTTP como 400 (BAD REQUEST)
+                response.getWriter().write("{\"message\": \"ID de anime no proporcionado.\"}");
+                return;
+            }
+
+            int idAnime = Integer.parseInt(idParam);
+
+            // Consulta SQL para eliminar una anime de la base de datos
+            String query = "DELETE FROM animes WHERE id_anime = ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            // Establecer los parámetros de la consulta de eliminación
+            statement.setInt(1, idAnime);
+
+            // Ejecutar la consulta de eliminación
+            int rowsDeleted = statement.executeUpdate();
+
+            if (rowsDeleted > 0) {
+                response.setStatus(HttpServletResponse.SC_OK); // Configurar el código de estado de la respuesta HTTP como 200 (OK)
+                response.getWriter().write("{\"message\": \"Anime eliminado exitosamente.\"}");
+            } else {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND); // Configurar el código de estado de la respuesta HTTP como 404 (NOT FOUND)
+                response.getWriter().write("{\"message\": \"Anime no encontrado.\"}");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Imprimir el error en caso de problemas con la base de datos
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // Configurar el código de estado de la respuesta HTTP como 500 (INTERNAL SERVER ERROR)
+        } catch (NumberFormatException e) {
+            e.printStackTrace(); // Imprimir el error en caso de problemas con el formato del número
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // Configurar el código de estado de la respuesta HTTP como 400 (BAD REQUEST)
+            response.getWriter().write("{\"message\": \"ID de anime inválido.\"}");
+        } finally {
+            conexion.close(); // Cerrar la conexión a la base de datos al finalizar la operación
         }
     }
 }
